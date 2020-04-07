b71f9c57117a6ac8a8d12f7a3e83bee8130526e4
1481169@mail.ru <1481169@mail.ru>
2020-04-07 12:42:04 +0300

    Добавлено взаимодействие с почтой

--- blog/models.py
+++ blog/models.py
@@ -6,7 +6,7 @@ from django.urls import reverse
 
 class PublishedManager(models.Manager):
     def get_queryset(self):
-        return super().get_queryset().filter(status='published')
+        return super(PublishedManager, self).get_queryset().filter(status='published')
 
 
 class Post(models.Model):
@@ -25,11 +25,12 @@ class Post(models.Model):
     object = models.Manager()
     published = PublishedManager()
 
-    def get_absolute_url(self):
-        return reverse('blog:post_detail', args=[self.publish.year, self.publish.month, self.publish.day, self.slug])
-
     class Meta:
         ordering = ('-publish',)
 
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=[self.publish.year, self.publish.month, self.publish.day, self.slug])
+
     def __str__(self):
         return self.title
+
